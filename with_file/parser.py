5847fc9998aeab8db02277a736077fe9efabd904
Denis Frolov <denisfrolov.coder@gmail.com>
2021-03-29 18:15:14 +0300

    fix with_file

--- with_file/parser.py
+++ with_file/parser.py
@@ -6,7 +6,19 @@ content = html.read()
 
 soup = BeautifulSoup(content, 'html.parser')
 
+title = soup.find_all('h1', class_='title')
+pharagraph = soup.find_all('p', class_='pharagraph')
+link = soup.find_all('a', class_='link')
 li = soup.find_all('li', class_='item')
+
+for t in title:
+    print('title:', t.get_text())
+
+for p in pharagraph:
+    print('p:', p.get_text())
+
+for l in link:
+    print('a:', l.get_text())
 
 for l in li:
     print('li:', l.get_text())
